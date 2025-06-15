eda3bdb6456f6cf74f7291fdb3f1420f0c48dc66
Shiryu_Go <ilovenhunhu777@gmail.com>
2025-06-15 12:17:06 +0900

    インポート文のフォーマットを修正
    使用していないインポートでした

--- src/chatApp.tsx
+++ src/chatApp.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import  { useState, useEffect, useRef } from "react";
 
 function ChatApp() {
   const [messages, setMessages] = useState<{ from: string; text: string }[]>([]);
